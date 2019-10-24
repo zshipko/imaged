09ac62b914dca1bd1b9a4c06c99e82d109e4ded3
Zach Shipko <zachshipko@gmail.com>
2019-10-23 20:32:40 -0700

    update

--- editor/src/main.rs
+++ editor/src/main.rs
@@ -2,7 +2,13 @@ use imaged_editor::*;
 
 pub fn main() -> Result<(), Error> {
     let args: Vec<String> = std::env::args().skip(1).collect();
+    if args.len() < 2 {
+        println!("USAGE: imaged-editor [ROOT] [KEY]");
+        return Ok(());
+    }
+
     let mut app = App::new(&args[0])?;
     app.open_window(&args[1])?;
+
     app.run()
 }
