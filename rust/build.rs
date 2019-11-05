93e4f7e42f169b623790fec7303a013dc08e8be4
Zach Shipko <zachshipko@gmail.com>
2019-11-04 21:20:34 -0800

    update

--- rust/build.rs
+++ rust/build.rs
@@ -1,7 +1,10 @@
 fn main() {
-    pkg_config::probe_library("babl").unwrap();
-    pkg_config::probe_library("ezimage").unwrap();
-    println!("cargo:rustc-link-lib=static=imaged");
     println!("cargo:rustc-link-search=native=..");
     println!("cargo:rustc-link-search=native=.");
+
+    println!("cargo:rustc-link-lib=static=imaged");
+
+    pkg_config::probe_library("babl").unwrap();
+    pkg_config::probe_library("ezimage").unwrap();
+    let _ = pkg_config::probe_library("libraw");
 }
