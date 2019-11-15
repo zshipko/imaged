8f36176e067752395474cb2532f14095b5d74e8f
Zach Shipko <zachshipko@gmail.com>
2019-11-15 11:31:11 -0800

    update

--- rust/src/conv.rs
+++ rust/src/conv.rs
@@ -1,6 +1,6 @@
 #![cfg(feature = "halide")]
 
-pub fn halide_buffer(im: &crate::Image) -> halide_runtime::Buffer {
+pub fn halide_buffer(im: &mut crate::Image) -> halide_runtime::Buffer {
     let meta = im.meta();
 
     // This only works because imaged Kind is modeled after halide
@@ -14,8 +14,23 @@ pub fn halide_buffer(im: &crate::Image) -> halide_runtime::Buffer {
     )
 }
 
-impl<'a> From<&'a crate::Image> for halide_runtime::Buffer {
-    fn from(x: &'a crate::Image) -> halide_runtime::Buffer {
+impl<'a> From<&'a mut crate::Image> for halide_runtime::Buffer {
+    fn from(x: &'a mut crate::Image) -> halide_runtime::Buffer {
         halide_buffer(x)
+    }
+}
+
+impl<'a> From<&'a crate::Image> for halide_runtime::Buffer {
+    fn from(im: &'a crate::Image) -> halide_runtime::Buffer {
+        let meta = im.meta();
+        let kind = unsafe { std::mem::transmute_copy(&meta.kind) };
+        halide_runtime::Buffer::new(
+            meta.width as i32,
+            meta.height as i32,
+            meta.channels() as i32,
+            halide_runtime::Type(kind, meta.bits),
+            im.data_ptr() as *mut u8, // Cheating by converting the const pointer to mut,
+                                      // this should only be used for const halide buffers
+        )
     }
 }
