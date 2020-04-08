b4d8e59cda732c83478e5c0c6ab5825598f222f2
Zach Shipko <zachshipko@gmail.com>
2020-04-07 21:00:07 -0700

    Try to get docs building

--- rust/build.rs
+++ rust/build.rs
@@ -1,16 +1,19 @@
 fn main() {
-    let make = std::env::var("MAKE").unwrap_or_else(|_| "make".to_string());
+    #[cfg(feature = "docs-rs")]
+    {
+        let make = std::env::var("MAKE").unwrap_or_else(|_| "make".to_string());
 
-    std::process::Command::new(make)
-        .current_dir("..")
-        .output()
-        .unwrap();
+        std::process::Command::new(make)
+            .current_dir("..")
+            .output()
+            .unwrap();
 
-    println!("cargo:rustc-link-search=native=..");
-    println!("cargo:rustc-link-search=native=.");
+        println!("cargo:rustc-link-search=native=..");
+        println!("cargo:rustc-link-search=native=.");
 
-    println!("cargo:rustc-link-lib=static=imaged");
-    pkg_config::probe_library("babl").unwrap();
-    pkg_config::probe_library("ezimage").unwrap();
-    let _ = pkg_config::probe_library("libraw");
+        println!("cargo:rustc-link-lib=static=imaged");
+        pkg_config::probe_library("babl").unwrap();
+        pkg_config::probe_library("ezimage").unwrap();
+        let _ = pkg_config::probe_library("libraw");
+    }
 }
